23bb4e5f00f156ee08d276a255a3f589e3dbd3c6
Eugene Solomonyk <solosoft2000@mail.ru>
2023-04-12 11:35:45 +0300

    feat: start work

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
@@ -9,8 +9,10 @@ const { NotImplementedError } = require('../extensions/index.js');
 class BinarySearchTree {
 
   root() {
-    throw new NotImplementedError('Not implemented');
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    console.log(Node);
+    return Node;
   }
 
   add(/* data */) {
